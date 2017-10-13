9fb3d7e1fc426bd474664ca61cfd5c1d24ba9e25
Christoph Küpker <christoph.kuepker@googlemail.com>
2017-10-13 23:53:08 +0200

    Added preview app

---
+++ de.inc47.SpellSheet.Preview/PdfPreview.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace de.inc47.SpellSheet.Preview
+{
+  /// <summary>
+  /// Interaction logic for PdfPreview.xaml
+  /// </summary>
+  public partial class PdfPreview : UserControl
+  {
+    private readonly int _columns, _rows;
+    private readonly int _gridsize = 20;
+
+    public PdfPreview()
+    {
+      InitializeComponent();
+      _columns =  (21 * 2) - 5;
+      _rows = (29 * 2) - 2;
+      Width = _columns * _gridsize;
+      Height = _rows * _gridsize;
+      InitGrid();
+    }
+
+    private void InitGrid()
+    {
+      for (int i = 0; i < _rows; i++)
+      {
+        var rd = new RowDefinition();
+        rd.Height = new GridLength(_gridsize);
+        PreviewGrid.RowDefinitions.Add(rd);
+        for (int j = 0; j < _columns; j++)
+        {
+          var cd = new ColumnDefinition();
+          cd.Width = new GridLength(_gridsize);
+          PreviewGrid.ColumnDefinitions.Add(cd);
+          Rectangle r = new Rectangle();
+          r.Width = _gridsize;
+          r.Height = _gridsize;
+          r.Fill = new SolidColorBrush(Colors.White);
+          r.Stroke = new SolidColorBrush(Colors.Gray);
+          Grid.SetRow(r, i);
+          Grid.SetColumn(r, j);
+          PreviewGrid.Children.Add(r);
+        }
+      }
+      
+    }
+  }
+}
