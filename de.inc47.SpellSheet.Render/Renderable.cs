eda24a769b18ac83e1ff40b4b22eff19c19ff8cb
Christoph Küpker <christoph.kuepker@ise.de>
2017-10-17 12:37:26 +0200

    Added template interfaces and block dimensions

---
+++ de.inc47.SpellSheet.Render/Renderable.cs
@@ -0,0 +1,18 @@
+﻿namespace de.inc47.SpellSheet.Render
+{
+  public abstract class Renderable : IRenderable
+  {
+    protected Renderable(int row, int column, int width, int height)
+    {
+      Row = row;
+      Column = column;
+      Width = width;
+      Height = height;
+    }
+
+    public int Column { get; }
+    public int Row { get; }
+    public int Width { get; }
+    public int Height { get; }
+  }
+}
