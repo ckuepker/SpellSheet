54f11b247131117fd43c225a01023480af6379b3
Christoph Küpker <christoph.kuepker@ise.de>
2017-10-17 15:42:11 +0200

    Added character's eigenschaften rendering in SpellTemplate

--- de.inc47.SpellSheet.Template/SpellTemplate.cs
+++ de.inc47.SpellSheet.Template/SpellTemplate.cs
@@ -1,6 +1,8 @@
 using System;
 using de.inc47.Spells;
+using de.inc47.Spells.Enumerations;
 using de.inc47.SpellSheet.Render;
+using de.inc47.SpellSheet.Render.Enum;
 
 namespace de.inc47.SpellSheet.Template
 {
@@ -12,7 +14,24 @@ namespace de.inc47.SpellSheet.Template
       var info = data.Item2;
       var root = new Block();
 
+      root.Children.Add(RenderEigenschaften(info));
+
       return root;
+    }
+
+    private Block RenderEigenschaften(ICharacterInformation character)
+    {
+      var block = new Block();
+      foreach (Eigenschaft e in Enum.GetValues(typeof(Eigenschaft)))
+      {
+        var eigenschaftsBlock = new Block();
+        var label = new Text(56, 0, 2, 1, e.ToString(), TextStyle.Label);
+        var text = new Text(57,0,2,1,character.GetEigenschaft(e).ToString(),TextStyle.Default);
+        eigenschaftsBlock.Children.Add(label);
+        eigenschaftsBlock.Children.Add(text);
+        block.Children.Add(eigenschaftsBlock);
+      }
+      return block;
     }
   }
 }
