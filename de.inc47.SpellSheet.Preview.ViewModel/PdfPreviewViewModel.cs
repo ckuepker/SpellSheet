f8aaef69adcc2812c5626d81030d81fa606e00c9
Christoph Küpker <christoph.kuepker@googlemail.com>
2017-10-18 23:41:23 +0200

    Formatted PdfPreviewViewModel

--- de.inc47.SpellSheet.Preview.ViewModel/PdfPreviewViewModel.cs
+++ de.inc47.SpellSheet.Preview.ViewModel/PdfPreviewViewModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace de.inc47.SpellSheet.Preview.ViewModel
 {
-    public class PdfPreviewViewModel : ViewModelBase, IPdfPreviewViewModel
-    {
-    }
+  public class PdfPreviewViewModel : ViewModelBase, IPdfPreviewViewModel
+  {
+  }
 }
